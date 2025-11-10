7e4de582c2eed3f88faedcf6d432f71becd4d7d4
gahyeon <janghyeon2412@gmail.com>
2025-11-10 11:32:17 +0900

    feat(hub): 허브 Swagger 설정 추가 및 API 문서화 적용 (#10)

--- src/main/java/com/logilink/hub/domain/hub/controller/HubController.java
+++ src/main/java/com/logilink/hub/domain/hub/controller/HubController.java
@@ -7,6 +7,8 @@ import com.logilink.hub.domain.hub.model.entity.Hub;
 import com.logilink.hub.domain.hub.repository.HubRepository;
 import com.logilink.hub.domain.hub.service.HubService;
 import com.logilink.hub.domain.hub.service.HubServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
+@Tag(name = "Hub API", description = "허브 관리 API")
 @RestController
 @RequestMapping("/api/v1/hubs")
 @RequiredArgsConstructor
@@ -26,32 +29,37 @@ public class HubController {
 
     private final HubService hubService;
 
+    @Operation(summary = "허브 생성", description = "새로운 허브 등록")
     @PostMapping
     public ResponseEntity<HubResponse> createHub(@Valid @RequestBody HubCreateRequest hubCreateRequest) {
         Hub hub = hubService.createHub(hubCreateRequest.toEntity());
         return ResponseEntity.ok(new HubResponse(hub));
     }
 
+    @Operation(summary = "허브 수정", description = "허브의 이름, 주소, 위도, 경도 수정")
     @PutMapping("/{hubId}")
     public ResponseEntity<HubResponse> updateHub(@PathVariable UUID hubId, @Valid @RequestBody HubUpdateRequest hubUpdateRequest) {
         Hub hub = hubService.updateHub(hubId, hubUpdateRequest.toEntity());
         return ResponseEntity.ok(new HubResponse(hub));
     }
 
+    @Operation(summary = "허브 삭제", description = "허브를 논리적으로 삭제")
     @DeleteMapping("/{hubId}")
     public ResponseEntity<Void> deleteHub(@PathVariable UUID hubId) {
         hubService.deleteHub(hubId);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "허브 단건 조회", description = "허브 ID로 단일 조회")
     @GetMapping("/{hubId}")
     public ResponseEntity<HubResponse> getHub(@PathVariable UUID hubId) {
         Hub hub = hubService.getHub(hubId);
         return ResponseEntity.ok(new HubResponse(hub));
     }
 
+    @Operation(summary = "허브 전체 조회", description = "검색어, 페이지, 정렬 조건을 기준으로 허브 조회")
     @GetMapping
-    public ResponseEntity<Page<HubResponse>> getAllHubs(
+    public ResponseEntity<Page<HubResponse>> getHubPage(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size,
             @RequestParam(defaultValue = "createdAt,desc") String sort,
@@ -75,4 +83,3 @@ public class HubController {
     }
 
 }
-
